bd99f277ff07de843a98d275a3f22517afc223fe
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2025-12-31 16:49:57 +0100

    fix: add missing type decs

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,10 +1,14 @@
 import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({
+      rollupTypes: true,
+      tsconfigPath: './tsconfig.app.json',
+    }) as never],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -1,10 +1,14 @@
 import { resolve } from 'node:path'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), dts({
+    rollupTypes: true,
+    tsconfigPath: './tsconfig.app.json',
+  }) as any],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
